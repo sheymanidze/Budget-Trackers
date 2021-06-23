a2057651e44860477a1ae1bffbd7692335736ebc
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-06-23 14:55:22 -0400

    db.js created

--- public/db.js
+++ public/db.js
@@ -3,3 +3,16 @@ let db;
 
 //Budget database
 const request = indexedDB.open("budgets", 1);
+
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+//if app online
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
