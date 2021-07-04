f34df25f629d6f63d4f3fb3e11bb4f5b63a71fa5
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-04 11:35:17 -0400

    service worker files to cache added

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+var CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+var FILES_TO_CACHE = [
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/",
+  "/db.js",
+  "/index.html",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/styles.css"
+];
